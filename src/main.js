02f74374e1f0219de521f3a4721cd808bfda6bbd
Colin Huang <colinshuang26@gmail.com>
2026-01-15 03:04:18 -0800

    add scenes

--- src/main.js
+++ src/main.js
@@ -4,6 +4,13 @@ config = {
     type: Phaser.AUTO,
     width: 640,
     height: 480,
+    scene: [ Menu, Play ]
 }
 
+
 let game = new Phaser.Game(config);
+
+
+// UI
+let borderUISize = game.config.height / 15
+let borderPadding = borderUISize / 3
